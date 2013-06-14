22eae30751d9d30650344379b209494740a5b7fa
dennisschagt <dennisschagt@gmail.com>
2013-06-14 14:03:56 +0200

    Added change weekprogram activity

--- Thermostaat/src/nl/tuestudent/thermostaat/ChangeWeekprogram.java
+++ Thermostaat/src/nl/tuestudent/thermostaat/ChangeWeekprogram.java
@@ -2,27 +2,20 @@ package nl.tuestudent.thermostaat;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.content.Intent;
 import android.view.Menu;
-import android.view.View;
 
-public class MainActivity extends Activity {
+public class ChangeWeekprogram extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
-	
-	public void btnChangeWeekProgram(View view) {
-		Intent intent = new Intent(this, ChangeWeekprogram.class);
-		startActivity(intent);
+		setContentView(R.layout.activity_change_weekprogram);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.change_weekprogram, menu);
 		return true;
 	}
 
