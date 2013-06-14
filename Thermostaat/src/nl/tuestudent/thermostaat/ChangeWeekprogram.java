c1fd215da0eff2ba720408eb218831e49627847c
dennisschagt <dennisschagt@gmail.com>
2013-06-14 14:21:08 +0200

    Added day overview activity

--- Thermostaat/src/nl/tuestudent/thermostaat/ChangeWeekprogram.java
+++ Thermostaat/src/nl/tuestudent/thermostaat/ChangeWeekprogram.java
@@ -2,14 +2,47 @@ package nl.tuestudent.thermostaat;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class ChangeWeekprogram extends Activity {
+	public final static String EXTRA_MESSAGE = "nl.tuestudent.thermostaat.MESSAGE";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_change_weekprogram);
+	}
+	
+	public void changeDay(View view) {
+		Intent intent = new Intent(this, ChangeDay.class);
+	    String message = "day: ";
+	    switch(view.getId()) {
+	    case R.id.button_change_sunday:
+	    	message += "Sunday";
+	    	break;
+	    case R.id.button_change_monday:
+	    	message += "Monday";
+	    	break;
+	    case R.id.button_change_tuesday:
+	    	message += "Tuesday";
+	    	break;
+	    case R.id.button_change_wednesday:
+	    	message += "Wednesday";
+	    	break;
+	    case R.id.button_change_thursday:
+	    	message += "Thursday";
+	    	break;
+	    case R.id.button_change_friday:
+	    	message += "Friday";
+	    	break;
+	    case R.id.button_change_saturday:
+	    	message += "Saturday";
+	    	break;
+	    }
+	    intent.putExtra(EXTRA_MESSAGE, message);
+	    startActivity(intent);
 	}
 
 	@Override
